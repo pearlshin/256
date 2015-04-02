37cb78b27f00ec6e5553bb9635aaeb0bef91db6d
Pearl Shin <pearlshin22@gmail.com>
2015-04-02 11:52:15 -0400

    create JS folder

---
+++ js/game.js
@@ -0,0 +1,12 @@
+// game model to store state of the game
+
+Game = function() {
+
+}
+
+// game = new Game() // generates a random starting board
+
+// game = new Game('0000202000000000')
+
+// add toString() method to Game model
+// game.toString();
